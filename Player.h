5674f9e64a82e1d061b1ded666414078a558c263
ReversibleLag <kaisouth123@gmail.com>
2022-04-09 02:32:54 -0400

    juice

--- Player.h
+++ Player.h
@@ -4,11 +4,23 @@
 class Player
 {
 private:
-
+	int hp;
+	int hpMax;
+	int level;
+	std::string city;
 
 public:
 
+	//accessors
+	inline const int& getHp() const { return this->hp; }
+	inline const int& getHpMax() const { return this->hpMax; }
+	inline const int& getLevel() const { return this->level; }
+	inline const std::string& getCity() const { return this->city; }
 
+
+	//setters
+	inline void setHp(const int hp) { this->hp = hp; }
+	void takeDamage(const int damage);
 
 };
 
